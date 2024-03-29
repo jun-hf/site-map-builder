0b5cc64284620b0674f4ae4ed6c49b4e832a2848
jun-hf <fong.junh@gmail.com>
2024-03-29 09:06:28 +0000

    draft implementation sitemap

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/jun-hf/site-map-builder
 
 go 1.22.1
 
-require github.com/jun-hf/link-extractor v0.0.0-20240327220621-2c8f5eda3adc
+require github.com/jun-hf/link-extractor v0.0.0-20240328154205-66e2c2219b36
 
 require golang.org/x/net v0.22.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,72 @@
 package main
 
 import (
-	"github.com/jun-hf/link-extractor"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	// "os"
+	"strings"
+	link "github.com/jun-hf/link-extractor/link"
 )
 
+type SiteMap struct {
+	href map[string]interface{}
+	domainName string // https://example.com/
+}
+
+func getHtml(url string) (string, error){
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	html, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	return string(html), nil
+}
+
+
+// // takes in the url and return back a string slice which containe the
+func (sm *SiteMap)buildSiteTree(url string) ([]link.Link, error) {
+	requestUrl := url
+	if !strings.HasPrefix(requestUrl, sm.domainName) {
+		requestUrl = sm.domainName + requestUrl
+	}
+	html, err := getHtml(requestUrl)
+	if err != nil {
+		log.Print(err)
+		return make([]link.Link, 0), err
+	}
+	links, _ := link.Parser(strings.NewReader(html))
+	return links, nil
+}
+
+// // Takes in a url and build a site map return in xml format
+func (sm *SiteMap)BuildSiteMap() { // (xml, error)
+	link, _ := sm.buildSiteTree(sm.domainName)
+	for _, l:= range link {
+		fmt.Printf(l.Href+ "\n")
+	}
+}
+
 func main() {
-	
+	// resp, err := http.Get("https://pkg.go.dev//net/http")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)
+	// }
+	// defer resp.Body.Close()
+	// html, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	sm := SiteMap{domainName: "https://pkg.go.dev//net/http#Head"}
+	sm.BuildSiteMap()
+
+	// fmt.Println(string(html))
 }
