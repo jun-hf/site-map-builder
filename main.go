a915d53b05074836585dc9aed5572ce912a74b29
jun-hf <fong.junh@gmail.com>
2024-03-29 15:19:39 +0000

    created xml

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	// "os"
+	"os"
 	"strings"
+
 	link "github.com/jun-hf/link-extractor/link"
 )
 
 type SiteMap struct {
-	href map[string]interface{}
+	href map[string]string
 	domainName string // https://example.com/
+	depth int
+	visitedDepth int
+}
+
+type url struct {
+	Loc string `xml:"loc"`
+}
+
+type urlSet struct {
+	Url []url `xml:"url"`
+	Xmlns string `xml:"xmlns,attr"`
 }
 
 func getHtml(url string) (string, error){
@@ -31,42 +44,65 @@ func getHtml(url string) (string, error){
 
 
 // // takes in the url and return back a string slice which containe the
-func (sm *SiteMap)buildSiteTree(url string) ([]link.Link, error) {
-	requestUrl := url
-	if !strings.HasPrefix(requestUrl, sm.domainName) {
-		requestUrl = sm.domainName + requestUrl
+func (sm *SiteMap)buildSiteTree(url string) error {
+	sm.visitedDepth++
+	if sm.depth < sm.visitedDepth {
+		return nil
 	}
-	html, err := getHtml(requestUrl)
+	html, err := getHtml(url)
 	if err != nil {
 		log.Print(err)
-		return make([]link.Link, 0), err
+		return err
 	}
 	links, _ := link.Parser(strings.NewReader(html))
-	return links, nil
-}
+	
 
-// // Takes in a url and build a site map return in xml format
-func (sm *SiteMap)BuildSiteMap() { // (xml, error)
-	link, _ := sm.buildSiteTree(sm.domainName)
-	for _, l:= range link {
-		fmt.Printf(l.Href+ "\n")
+	for _, l := range links {
+		if strings.HasPrefix(l.Href, "#") {
+			continue
+		}
+		if (strings.HasPrefix(l.Href, "/") && l.Href != "/") || (strings.HasPrefix(l.Href, "?") && l.Href != "?"){
+			l.Href = sm.domainName + l.Href
+		}
+		if !strings.HasPrefix(l.Href, sm.domainName) {
+			continue
+		}
+		if _, ok := sm.href[l.Href]; ok {
+			continue
+		}
+		sm.href[l.Href] = "member"
+		if err := sm.buildSiteTree(l.Href); err != nil {
+			fmt.Println(err)
+		}
 	}
+	return nil
+}
+func (sm *SiteMap) createXML() ([]byte, error) {
+	urlList := make([]url,0)
+	for hr, _ := range sm.href {
+		u := url{Loc:hr}
+		urlList = append(urlList, u)
+	}
+	urlS := urlSet{urlList, "http://www.sitemaps.org/schemas/sitemap/0.9"}
+	output, err := xml.MarshalIndent(urlS, " ", "  ")
+	if err != nil {
+		return nil, err
+	}
+	return []byte(xml.Header + string(output)), nil
+
+}
+// // Takes in a url and build a site map return in xml format
+func (sm *SiteMap)BuildSiteMap() ([]byte, error){// (xml, error)
+	sm.buildSiteTree(sm.domainName)
+	return sm.createXML()
 }
 
 func main() {
-	// resp, err := http.Get("https://pkg.go.dev//net/http")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-	// defer resp.Body.Close()
-	// html, err := io.ReadAll(resp.Body)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+	sm := SiteMap{domainName: "https://pkg.go.dev/net/http", href: make(map[string]string), depth: 3}
+	output, err := sm.BuildSiteMap()
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Stdout.Write(output)
 
-	sm := SiteMap{domainName: "https://pkg.go.dev//net/http#Head"}
-	sm.BuildSiteMap()
-
-	// fmt.Println(string(html))
 }
